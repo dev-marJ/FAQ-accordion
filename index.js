23a1ee69567065ac2236ad15165951955020ff07
dev-marJ <152494760+dev-marJ@users.noreply.github.com>
2023-12-27 15:11:05 +0200

    finished with JS part

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const questionBtn = document.querySelectorAll("button");
 const dropdownText = document.querySelectorAll(".dropdown-text");
-const plusMinusImg = document.querySelector(".plus-minus-img");
+const plusMinusImgs = document.querySelectorAll(".plus-minus-img");
 
 questionBtn.forEach((button, index) => {
   button.addEventListener("click", function () {
+    const plusMinusImg = button.querySelector(".plus-minus-img");
+
     dropdownText.forEach((dropdown, dropdownIndex) => {
       if (index === dropdownIndex) {
-        // plusMinusImg.src = "/assets/images/icon-minus.svg"
-        if (dropdown.style.display === "none" || dropdown.style.display === "") {
-            dropdown.style.display = "block";
+        if (
+          dropdown.style.display === "none" ||
+          dropdown.style.display === ""
+        ) {
+          dropdownText.forEach((item, i) => {
+            if (i !== index) {
+              item.style.display = "none";
+              plusMinusImgs[i].src = "/assets/images/icon-plus.svg";
+            }
+          });
+          dropdown.style.display = "block";
+          plusMinusImg.src = "/assets/images/icon-minus.svg";
         } else {
-            dropdown.style.display = "none";
+          dropdown.style.display = "none";
+          plusMinusImg.src = "/assets/images/icon-plus.svg";
         }
-    } else {
+      } else {
         dropdown.style.display = "none";
-    }
+      }
     });
   });
 });
